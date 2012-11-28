7c821f693cc2a55207c0b50c839a4eafd25e23ac
Magnus Stahl <color.ui>
2012-11-28 15:36:55 +0100

    Refactor AssemblyInfo for Model

--- SliceOfPie_BDSA/Properties/AssemblyInfo.cs
+++ SliceOfPie_BDSA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SliceOfPie_BDSA")]
+[assembly: AssemblyTitle("SliceOfPie_Model")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SliceOfPie_BDSA")]
+[assembly: AssemblyProduct("SliceOfPie_Model")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
