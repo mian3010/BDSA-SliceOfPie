2e543a209182698c442a64ac64c743f602161469
Claus L. Henriksen <clih@itu.dk>
2012-11-28 16:08:07 +0100

    Created a project with a main method for trying things out

---
+++ SliceOfTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SliceOfPie_Model;
+
+namespace SliceOfTest {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Adding user");
+      Context.AddUser("Hejsa");
+      User user = Context.GetUsers().First();
+      Console.WriteLine("User: " + user.email);
+    }
+  }
+}
