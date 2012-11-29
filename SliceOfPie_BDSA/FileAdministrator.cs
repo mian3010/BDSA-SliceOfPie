96335ab97ddc95911548b9643d4de912a2eb8057
Michael Soeby Andersen <michael@mileratec.com>
2012-11-29 10:33:35 +0100

    Fixed compilation errors

--- SliceOfPie_BDSA/FileAdministrator.cs
+++ SliceOfPie_BDSA/FileAdministrator.cs
@@ -3,40 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SliceOfPie_Model
-{
-    public class FileAdministratorAdaptor : IAdministrator
-    {
-        private ICommunicator communicator;
-        public delegate void FileEventHandler(object sender, File file);
-        public event FileEventHandler FilesUpdated, ContentAdded, FileSaved;
+namespace SliceOfPie_Model {
+  public class FileAdministratorAdaptor : IAdministrator {
+    private ICommunicator communicator;
+    public delegate void FileEventHandler(object sender, File file);
+    public event FileEventHandler FilesUpdated, ContentAdded, FileSaved;
 
-        //singleton instance
-        public static FileAdministratorAdaptor administrator;
+    //singleton instance
+    public static FileAdministratorAdaptor administrator;
 
-        private FileAdministratorAdaptor()
-        { 
-            
-        }
-
-        public static FileAdministratorAdaptor GetInstance()
-        {
-            if (administrator == null)
-                administrator = new FileAdministratorAdaptor();
-            return administrator;    
-        }
-
-        public void SaveFile(File file) { 
-            bool b = communicator.SaveFile(file);
-            if (b)
-                FileSaved(this, file);
-        }
-
-        public void OpenFile(File file)
-        { 
-            
-        }
-
+    private FileAdministratorAdaptor() {
 
     }
+
+    public static FileAdministratorAdaptor GetInstance() {
+      if (administrator == null)
+        administrator = new FileAdministratorAdaptor();
+      return administrator;
+    }
+
+    public void SaveFile(File file) {
+      bool b = communicator.SaveFile(file);
+      if (b)
+        FileSaved(this, file);
+    }
+
+    public void OpenFile(File file) {
+
+    }
+
+    public void GetAllFiles() {
+      throw new NotImplementedException();
+    }
+
+  }
 }
