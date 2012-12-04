5d0d2e7ace06ab366fc8aae1c2c55bda2179a9cd
tokeloke <tokeloke@gmail.com>
2012-12-04 19:00:32 +0100

    Marshaller working and tested

--- SliceOfPie_Testing/HTMLMarshallerTest.cs
+++ SliceOfPie_Testing/HTMLMarshallerTest.cs
@@ -4,6 +4,8 @@ using SliceOfPie_Model;
 using SliceOfPie_Network;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Diagnostics;
+
 namespace SliceOfPie_Testing
 {
     [TestClass]
@@ -24,8 +26,34 @@ namespace SliceOfPie_Testing
             log.Add(log4);
 
             string xml= HTMLMarshaller.MarshallLog(log);
-            Console.Out.WriteLine(xml);
+            Console.Out.WriteLine("XML: " + xml);
+            Assert.AreNotEqual(null, xml);
+        }
 
+        [TestMethod]
+        public void TestUnMarshallLog()
+        {
+            List<LogEntry> log = new List<LogEntry>();
+
+            LogEntry log1 = new LogEntry(1, "file1", "/local", DateTime.Now, FileModification.Add);
+            LogEntry log2 = new LogEntry(2, "file2", "/local", DateTime.Now, FileModification.Delete);
+            LogEntry log3 = new LogEntry(3, "file3", "/local", DateTime.Now, FileModification.Modify);
+            LogEntry log4 = new LogEntry(4, "file4", "/local", DateTime.Now, FileModification.Add);
+            log.Add(log1);
+            log.Add(log2);
+            log.Add(log3);
+            log.Add(log4);
+
+            string xml= HTMLMarshaller.MarshallLog(log);
+            List<LogEntry> list = HTMLMarshaller.UnMarshallLog(xml);
+
+            for (int i = 0; i < list.Count; i++ )
+            {
+                LogEntry entry = list[i];
+                LogEntry refEntry = log[i];
+                Assert.AreEqual(entry.id, refEntry.id);
+                Console.Out.WriteLine("entry: " + entry.id);
+            }
         }
     }
 }
