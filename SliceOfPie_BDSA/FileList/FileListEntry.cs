564627791059573825e9784c1c8854fda89f7c46
Magnus Stahl <magnus@stahl.dk>
2012-12-06 16:58:55 +0100

    Public filelist

--- SliceOfPie_BDSA/FileList/FileListEntry.cs
+++ SliceOfPie_BDSA/FileList/FileListEntry.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SliceOfPie_Model.FileList {
+namespace SliceOfPie_Model {
   class FileListEntry {
-    float Id { get; set; }
-    float Version { get; set; }
-    string Path { get; set; }
-    string Name { get; set; }
-    FileListType Type { get; set; }
-    bool IsDeleted { get; set; }
+    public long Id { get; set; }
+    public float Version { get; set; }
+    public string Path { get; set; }
+    public string Name { get; set; }
+    public FileListType Type { get; set; }
+    public bool IsDeleted { get; set; }
   }
 }
