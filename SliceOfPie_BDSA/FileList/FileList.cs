b0ba9ed3c2286d2e5b89831e9859c7b8a3551c13
Michael Soeby Andersen <michael@mileratec.com>
2012-12-06 16:13:28 +0100

    FileList added

---
+++ SliceOfPie_BDSA/FileList/FileList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SliceOfPie_Model.FileList {
+  class FileList {
+    IDictionary<long, FileListEntry> List { get; set; }
+  }
+}
---
+++ SliceOfPie_BDSA/FileList/FileListEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SliceOfPie_Model.FileList {
+  class FileListEntry {
+    float Version { get; set; }
+    string Path { get; set; }
+    string Name { get; set; }
+    FileListType Type { get; set; }
+    bool IsDeleted { get; set; }
+  }
+}
