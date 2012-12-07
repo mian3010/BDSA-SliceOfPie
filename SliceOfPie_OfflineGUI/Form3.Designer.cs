878a27cc67932c49bf3e93063c62a498444d8ba9
Michael Soeby Andersen <michael@mileratec.com>
2012-12-07 15:32:16 +0100

    Change

--- SliceOfPie_OfflineGUI/Form3.Designer.cs
+++ SliceOfPie_OfflineGUI/Form3.Designer.cs
@@ -28,47 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.treeView2 = new System.Windows.Forms.TreeView();
-            this.SuspendLayout();
-            // 
-            // treeView1
-            // 
-            this.treeView1.Location = new System.Drawing.Point(21, 52);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(305, 307);
-            this.treeView1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(359, 52);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // treeView2
-            // 
-            this.treeView2.Location = new System.Drawing.Point(385, 162);
-            this.treeView2.Name = "treeView2";
-            this.treeView2.Size = new System.Drawing.Size(121, 97);
-            this.treeView2.TabIndex = 2;
-            // 
-            // Form3
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(547, 386);
-            this.Controls.Add(this.treeView2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.treeView1);
-            this.Name = "Form3";
-            this.Text = "Form3";
-            this.Load += new System.EventHandler(this.Form3_Load);
-            this.ResumeLayout(false);
+      this.treeView1 = new System.Windows.Forms.TreeView();
+      this.button1 = new System.Windows.Forms.Button();
+      this.treeView2 = new System.Windows.Forms.TreeView();
+      this.SuspendLayout();
+      // 
+      // treeView1
+      // 
+      this.treeView1.Location = new System.Drawing.Point(21, 52);
+      this.treeView1.Name = "treeView1";
+      this.treeView1.Size = new System.Drawing.Size(305, 307);
+      this.treeView1.TabIndex = 0;
+      this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(359, 52);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // treeView2
+      // 
+      this.treeView2.Location = new System.Drawing.Point(385, 162);
+      this.treeView2.Name = "treeView2";
+      this.treeView2.Size = new System.Drawing.Size(121, 97);
+      this.treeView2.TabIndex = 2;
+      // 
+      // Form3
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(547, 386);
+      this.Controls.Add(this.treeView2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.treeView1);
+      this.Name = "Form3";
+      this.Text = "Form3";
+      this.Load += new System.EventHandler(this.Form3_Load);
+      this.ResumeLayout(false);
 
         }
 
--- SliceOfPie_OfflineGUI/Form3.cs
+++ SliceOfPie_OfflineGUI/Form3.cs
@@ -9,71 +9,85 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
 
-namespace SliceOfPie_OfflineGUI
-{
-    public partial class Form3 : Form
-    {
-        String xml = "<?xml version=\"1.0\"?><folder><file>Example.txt</file></folder>";
+namespace SliceOfPie_OfflineGUI {
+  public partial class Form3 : Form {
+    List<String> paths = new List<String>();
 
-        public Form3()
-        {
-            InitializeComponent();
-            //InitializeTree();
-        }
-
-        private void InitializeTree()
-        {
-            XmlDocument dom = new XmlDocument();
-            dom.LoadXml(xml);
-
-            treeView1.Nodes.Clear();
-            treeView1.Nodes.Add(new TreeNode(dom.DocumentElement.Name));
-            TreeNode tNode = new TreeNode();
-            tNode = treeView1.Nodes[0];
-
-            AddNode(dom.DocumentElement, tNode);
-            treeView1.ExpandAll();
-
-
-        }
-
-        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
-        {
-            XmlNode xNode;
-            TreeNode tNode;
-            XmlNodeList nodeList;
-            int i;
-
-            // Loop through the XML nodes until the leaf is reached.
-            // Add the nodes to the TreeView during the looping process.
-            if (inXmlNode.HasChildNodes)
-            {
-                nodeList = inXmlNode.ChildNodes;
-                for (i = 0; i <= nodeList.Count - 1; i++)
-                {
-                    xNode = inXmlNode.ChildNodes[i];
-                    inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
-                    tNode = inTreeNode.Nodes[i];
-                    AddNode(xNode, tNode);
-                }
-            }
-            else
-            {
-                // Here you need to pull the data from the XmlNode based on the
-                // type of node, whether attribute values are required, and so forth.
-                inTreeNode.Text = (inXmlNode.OuterXml).Trim();
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            InitializeTree();
-        }
-
-        private void Form3_Load(object sender, EventArgs e)
-        {
-
-        }
+    /// <summary>
+    /// To-Do delete
+    /// </summary>
+    private void TestPaths() {
+      paths.Add("C:/File");
+      paths.Add("C:/John");
+      paths.Add("C:/Fail");
+      paths.Add("C:/Fail/Example");
+      paths.Add("C:/Fail.txt");
+      paths.Add("C:/Fail/noob.txt");
+      paths.Add("D:/Fail/noob.txt");
     }
+
+    public Form3() {
+      InitializeComponent();
+      //InitializeTree();
+    }
+
+    private void InitializeTree() {
+      TestPaths();
+      TreeNode root = new TreeNode("Files");
+      TreeNode node = root;
+      treeView1.Nodes.Add(root);
+
+      foreach (string filePath in paths) {
+        node = root;
+        foreach (string pathBits in filePath.Split('/')) {
+          node = AddNode(node, pathBits);
+        }
+      }
+      treeView1.ExpandAll();
+
+    }
+    private TreeNode AddNode(TreeNode node, string key) {
+      if (node.Nodes.ContainsKey(key)) {
+        return node.Nodes[key];
+      } else {
+        return node.Nodes.Add(key, key);
+      }
+    }
+
+    private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode) {
+      XmlNode xNode;
+      TreeNode tNode;
+      XmlNodeList nodeList;
+      int i;
+
+      // Loop through the XML nodes until the leaf is reached.
+      // Add the nodes to the TreeView during the looping process.
+      if (inXmlNode.HasChildNodes) {
+        nodeList = inXmlNode.ChildNodes;
+        for (i = 0; i <= nodeList.Count - 1; i++) {
+          xNode = inXmlNode.ChildNodes[i];
+          inTreeNode.Nodes.Add(new TreeNode(xNode.Name));
+          tNode = inTreeNode.Nodes[i];
+          AddNode(xNode, tNode);
+        }
+      } else {
+        // Here you need to pull the data from the XmlNode based on the
+        // type of node, whether attribute values are required, and so forth.
+        inTreeNode.Text = (inXmlNode.OuterXml).Trim();
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e) {
+      InitializeTree();
+    }
+
+    private void Form3_Load(object sender, EventArgs e) {
+
+    }
+
+    private void treeView1_AfterSelect(object sender, TreeViewEventArgs e) {
+
+    }
+  }
 }
 
