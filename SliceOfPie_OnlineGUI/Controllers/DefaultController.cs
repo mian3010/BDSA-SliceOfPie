f2392df2c8b107b7c739500a1baf633cebd43e97
Michael Soeby Andersen <michael@mileratec.com>
2012-12-14 13:59:38 +0100

    Wokring on online

--- SliceOfPie_OnlineGUI/Controllers/DefaultController.cs
+++ SliceOfPie_OnlineGUI/Controllers/DefaultController.cs
@@ -4,21 +4,25 @@ using SliceOfPie_Model.Persistence;
 
 namespace SliceOfPie_OnlineGUI.Controllers {
   public class DefaultController : Controller {
-    public ActionResult Index()
-    {
-      @ViewBag.FileList = Models.FileModel.FileListToTree(Context2.GetFiles("test@example.com"));
+    public ActionResult Index() {
+      @ViewBag.FileList = Models.FileModel.FileListToTree(Context2.GetFiles("testuser0@example.com"));
       return View();
     }
 
     public ActionResult Editor() {
-      Document documentToEdit = Models.FileModel.GetFile(0);
-      @ViewBag.DocumentTitle = documentToEdit.Title;
-      @ViewBag.DocumentContent = documentToEdit.Content;
+      Document documentToEdit = Models.FileModel.GetDocument(0);
+      if (documentToEdit != null) {
+        @ViewBag.DocumentTitle = documentToEdit.Title;
+        @ViewBag.DocumentContent = documentToEdit.Content;
+      } else {
+        @ViewBag.DocumentTitle = "N/A";
+        @ViewBag.DocumentContent = "Document not found or not a document";
+      }
       return View();
     }
 
     public ActionResult Viewer() {
-      FileInstance fileToView = Models.FileModel.GetFile(0);
+      FileInstance fileToView = Models.FileModel.GetFile(int.Parse(Request.Params["id"]));
       @ViewBag.Document = fileToView.ToString();
       return View();
     }
@@ -29,7 +33,7 @@ namespace SliceOfPie_OnlineGUI.Controllers {
         var documentToView = (Document)fileToView;
         @ViewBag.Title = documentToView.Title;
       } catch (InvalidCastException) {
-        @ViewBag.Title = "File: "+fileToView.File.name;
+        @ViewBag.Title = "File: " + fileToView.File.name;
       }
       @ViewBag.DocumentHistory = fileToView.HistoryToString();
       return View();
